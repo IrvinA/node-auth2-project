0b01127807cb43d23b3a993a9faf6fb7568d645a
IrvinA <arevalosirvin@gmail.com>
2021-12-14 10:10:02 -0800

    auth router complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -17,25 +17,28 @@ router.post('/register', validateRoleName, (req, res, next) => {
 });
 
 router.post('/login', checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
+  if (bcrypt.compareSync(req.body.password,  req.user.password)) {
+    const token = buildToken(req.user)
+    res.status(200).json({
+      message: `${req.user.username} is back!`,
+      token,
+    })
+  } else {
+    next({ status: 401, message: 'Invalid credentials'})
+  }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role: user.role_name,
+  };
+  const options = {
+    exp: '1d',
+    iat: Date.now(),
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
